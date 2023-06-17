2c47c9142da7cd75751c872432646cba7d488372
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-06-17 11:07:32 +0530

    Set up complete frontend application

---
+++ client/src/constants.js
@@ -0,0 +1,16 @@
+export const APP_CONSTANTS = {
+  ALCHEMY_KEY:
+    "wss://virulent-red-tent.matic-testnet.discover.quiknode.pro/4e1afeddceb33ecded6e34b04d18bf4539ab6df8/",
+  CONTRACT_ADDRESS: "0x03EEe58C59bCF52BcA8E77fb788370FE3FdA0Bb7",
+  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+  CLIENT_ID:
+    "BKysQno7pBNfKwptl_uhB6Cd-dgEYnqgovvpneWj_N5desA7PmruvJENcG5rMcRqOZ2cP8LRFyYTDzjS6ceAaog",
+  RPC_TARGET:
+    "https://virulent-red-tent.matic-testnet.discover.quiknode.pro/4e1afeddceb33ecded6e34b04d18bf4539ab6df8/",
+  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+  ADAPTER_TWITTER_CLIENT_VERIFIER: "moonman-twitter-verse-1",
+  ADAPTER_TWITTER_CLIENT_ID: "UMpo2OHmDXHmgnsyKt75VWxtpiyBxCSc",
+  AUTH0_DOMAIN: "https://moonman369.us.auth0.com",
+  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+  REACT_APP_REFRESH_TIMER: 30,
+};
