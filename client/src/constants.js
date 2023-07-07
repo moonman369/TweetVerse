31bee3fdb1d68bd65b51ee1b4a4b5f23c5a449cc
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-07 12:39:01 +0530

    Change Web3Auth Client Id

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,7 +4,7 @@ export const APP_CONSTANTS = {
   CONTRACT_ADDRESS: "0x03EEe58C59bCF52BcA8E77fb788370FE3FdA0Bb7",
   OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
   CLIENT_ID:
-    "BNsuQid8SaxMdN3QS1lxXhVgD9M3JG-jRUDdejWuUS9Att9g-BVo9Zrdw23w0FA8tc57CMDVZm4T_hTX36QDUoU",
+    "BNdtANK7ZnXSCLQCjB0xduyldG4gVocoTN7pES25AOd-X5P_l7Q7Ye5w294xA88rygFzNKtmNdkyldYdHhUitOQ",
   RPC_TARGET:
     "https://virulent-red-tent.matic-testnet.discover.quiknode.pro/4e1afeddceb33ecded6e34b04d18bf4539ab6df8/",
   APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
